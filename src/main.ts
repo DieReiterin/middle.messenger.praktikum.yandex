8afa5cfdfdc1d68614e809d9983a616650c589c5
Васильев Серафим Дмитриевич <VasilyevSD@mlife.local>
2024-06-26 17:58:56 +0300

    Component-Page-Layout system reworked for Block use 2.2.0

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,338 @@
-import Block from "./tools/Block";
+import "./style.scss";
+import Handlebars from "handlebars";
+import Block from "./tools/Block.ts";
+
+import "./components/button/button.scss";
+import "./components/link/link.scss";
+import "./components/page-title/page-title.scss";
+import "./components/input-field/input-field.scss";
+import "./components/server-alert/server-alert.scss";
+import "./components/footer/footer.scss";
+
+import "./pages/login-page/login-page.scss";
+import "./pages/not-found-page/not-found-page.scss";
+import "./layouts/layout-navigated/layout-navigated.scss";
+
+class Button extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            events: {
+                click: () => console.log("event"),
+            },
+            attr: {
+                class: `button`,
+            },
+        });
+    }
+    render() {
+        return `<button class="{{className}}" type="{{type}}" page="{{page}}">{{text}}</button>`;
+    }
+}
+class Link extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            events: {
+                click: () => console.log("link-event"),
+            },
+            attr: {
+                class: `link`,
+            },
+        });
+    }
+    render() {
+        return `<a
+                    href="{{href}}"
+                    class="{{className}}"
+                    page="{{page}}"
+                    action="{{action}}"
+                >{{text}}</a>`;
+    }
+}
+class PageTitle extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            attr: {
+                class: `page-title`,
+            },
+        });
+    }
+    render() {
+        return `<h1 class="page-title {{className}}">
+                    {{text}}
+                </h1>`;
+    }
+}
+class InputField extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            attr: {
+                class: `input-field`,
+            },
+        });
+    }
+    render() {
+        return `<div class="{{className}} {{#if typeProfile}}input-field_type-profile{{/if}}">
+                    {{#if label}}
+                        <label class="input-field__label" for="{{id}}">
+                            {{label}}
+                        </label>
+                    {{/if}}
+                    <input
+                        name="{{name}}"
+                        class="input-field__control"
+                        type="text"
+                        id="{{id}}"
+                        placeholder="{{placeholder}}"
+                        value="{{value}}"
+                    />
+                    <label class="input-field__error" for="{{id}}">
+                        {{error}}
+                    </label>
+                </div>
+                `;
+    }
+}
+
+class ServerAlert extends Block {
+    constructor(props?, children?) {
+        super({
+            ...props,
+            attr: {
+                class: `server-alert`,
+            },
+            content: children.content,
+        });
+    }
+    render() {
+        return `{{#> ServerAlert }}
+                    {{{content}}}
+                {{/ ServerAlert }}`;
+        // return `<div class="server-alert {{ className }}">
+        //             {{{content}}}
+        //         </div>`;
+    }
+}
+class Footer extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            btn1: new Button({
+                className: "footer__btn",
+                page: "login",
+                text: "Авторизация",
+            }),
+            btn2: new Button({
+                className: "footer__btn",
+                page: "signin",
+                text: "Регистрация",
+            }),
+            btn3: new Button({
+                className: "footer__btn",
+                page: "chat",
+                text: "Список чатов",
+            }),
+            btn4: new Button({
+                className: "footer__btn",
+                page: "profile",
+                text: "Профиль",
+            }),
+            btn5: new Button({
+                className: "footer__btn",
+                page: "settings",
+                text: "Настройки",
+            }),
+            btn6: new Button({
+                className: "footer__btn",
+                page: "not-found",
+                text: "Страница 404",
+            }),
+            btn7: new Button({
+                className: "footer__btn",
+                page: "server-error",
+                text: "Страница 5**",
+            }),
+        });
+    }
+    render() {
+        return `<nav class="footer {{ className }}">
+                    <div class="footer__content">
+                        {{{btn1}}} 
+                        {{{btn2}}} 
+                        {{{btn3}}} 
+                        {{{btn4}}} 
+                        {{{btn5}}} 
+                        {{{btn6}}} 
+                        {{{btn7}}} 
+                    </div>
+                </nav>`;
+    }
+}
+class NotFoundPage extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            alert: new ServerAlert({
+                content: {
+                    title1: new PageTitle({
+                        className: "server-alert__title",
+                        text: "400",
+                    }),
+                    title2: new PageTitle({
+                        className: "server-alert__subtitle",
+                        text: "Не туда попали",
+                    }),
+                    link: new Link({
+                        className: "server-alert__link",
+                        page: "chat",
+                        text: "Назад к чатам",
+                    }),
+                },
+            }),
+        });
+    }
+    override render() {
+        return `<div class="not-found-page">
+                    {{{alert}}}
+                </div>`;
+    }
+}
+
+class LoginPage extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            title: new PageTitle({
+                className: "login-page__title",
+                text: "Вход",
+            }),
+            input1: new InputField({
+                className: "login-page__input",
+                label: "Логин",
+                placeholder: "введите логин",
+                error: "",
+                name: "login",
+                id: "login",
+            }),
+            input2: new InputField({
+                className: "login-page__input",
+                label: "Пароль",
+                placeholder: "введите пароль",
+                error: "Неверный пароль",
+                name: "password",
+                id: "password",
+            }),
+            btn: new Button({
+                className: "login-page__submit-btn",
+                text: "Войти",
+                page: "chat",
+            }),
+            link: new Link({
+                className: "login-page__link",
+                text: "Регистрация",
+                page: "signin",
+            }),
+        });
+    }
+    override render() {
+        return `<form class="login-page">
+                    <div class="login-page__main">
+                        {{{title}}} 
+                        {{{input1}}}  
+                        {{{input2}}}                          
+                    </div>
+                    <div class="login-page__footer">
+                        {{{btn}}}    
+                        {{{link}}}           
+                    </div>
+                </form>`;
+    }
+}
+
+class LayoutNavigated extends Block {
+    constructor(props?) {
+        super({
+            ...props,
+            footer: new Footer({
+                className: "layout-navigated__footer",
+            }),
+        });
+    }
+    override render() {
+        return `<main class="layout-navigated">
+                    <div class="layout-navigated__content" id="layout-content">
+                    </div>
+
+                    {{{footer}}}
+                </main>`;
+    }
+}
+function setLayout(type) {
+    const container = document.getElementById("app")!;
+    container.append(type.getContent()!);
+}
+function navigate(page) {
+    const content = document.getElementById("layout-content");
+    content.append(page.getContent()!);
+}
+const onLoadLayout = new LayoutNavigated();
+// const onLoadPage = new LoginPage();
+const notfound = new NotFoundPage();
+setLayout(onLoadLayout);
+navigate(notfound);
+
+// class Input extends Block {
+//     constructor(props) {
+//         super({
+//             ...props,
+//             events: {
+//                 change: (e: Event) =>
+//                     props.onChange((e.target as HTMLInputElement).value),
+//                 blur: () => this.validate(),
+//             },
+//         });
+//     }
+//     render() {
+//         return `<input />`;
+//     }
+//     validate() {
+//         console.log("Here we call validation code on blur");
+//     }
+// }
+
+// class PageWithButton extends Block {
+//     constructor(props) {
+//         super({
+//             ...props, //{buttonText: 'Button'}
+//             button: new Button({
+//                 text: props.buttonText,
+//             }),
+//             input: new Input({
+//                 label: "input",
+//                 onChange: (value) => {
+//                     this.setProps({ buttonText: value });
+//                 },
+//             }),
+//         });
+//     }
+//     componentDidUpdate(oldProps, newProps) {
+//         if (oldProps.buttonText !== newProps.buttonText) {
+//             this.children.button.setProps({ text: newProps.buttonText });
+//         }
+//         return true;
+//     }
+//     override render() {
+//         return "<div>{{{ button }}} {{{ input }}}</div>";
+//     }
+// }
+
+// const block = new PageWithButton({ buttonText: "Войти" });
+// //const block = new PageWithList();
+// const container = document.getElementById("app")!;
+// container.append(block.getContent()!);
 
 // class ChatItem extends Block {
 //     constructor({ ...props }) {
@@ -15,72 +349,6 @@ import Block from "./tools/Block";
 //       </div>`;
 //     }
 // }
-
-class Button extends Block {
-    constructor(props) {
-        super({
-            ...props,
-            events: {
-                click: () => console.log("event"),
-            },
-            attr: {
-                class: `fake`,
-            },
-        });
-    }
-
-    render() {
-        return "<button>{{text}}</button>";
-    }
-}
-
-class Input extends Block {
-    constructor(props) {
-        super({
-            ...props,
-            events: {
-                change: (e: Event) =>
-                    props.onChange((e.target as HTMLInputElement).value),
-                blur: () => this.validate(),
-            },
-        });
-    }
-
-    render() {
-        return `<input />`;
-    }
-
-    validate() {
-        console.log("Here we call validation code on blur");
-    }
-}
-
-class PageWithButton extends Block {
-    constructor(props) {
-        super({
-            ...props, //{buttonText: 'Button'}
-            button: new Button({ text: props.buttonText }),
-            input: new Input({
-                label: "input",
-                onChange: (value) => {
-                    this.setProps({ buttonText: value });
-                },
-            }),
-        });
-    }
-
-    componentDidUpdate(oldProps, newProps) {
-        if (oldProps.buttonText !== newProps.buttonText) {
-            this.children.button.setProps({ text: newProps.buttonText });
-        }
-        return true;
-    }
-
-    override render() {
-        return "<div>{{{ button }}} {{{ input }}}</div>";
-    }
-}
-
 // class PageWithList extends Block {
 //     constructor(props) {
 //         super({
@@ -116,11 +384,6 @@ class PageWithButton extends Block {
 //         return "<div>{{{ lists }}}</div>";
 //     }
 // }
-
-const block = new PageWithButton({ buttonText: "Button" });
-//const block = new PageWithList();
-const container = document.getElementById("app")!;
-container.append(block.getContent()!);
 
 // import Handlebars from "handlebars";
 // import * as Components from "./components";
