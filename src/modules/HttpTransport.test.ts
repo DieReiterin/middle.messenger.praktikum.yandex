f512cdbe652d47633c70a1c96702a0008ac7d9b9
Serafim Vasilev <zzerafim@ya.ru>
2024-08-27 11:11:27 +0300

    Test env setup, rimraf added 4.0.2

---
+++ src/modules/HttpTransport.test.ts
@@ -0,0 +1,27 @@
+import { expect, use } from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { createSandbox, SinonStub } from 'sinon';
+import HttpTransport from './HttpTransport';
+
+describe('Http Transport test suite', () => {
+    use(sinonChai.default);
+    const sandbox = createSandbox();
+    let http: HttpTransport;
+    let request: SinonStub<any>;
+
+    beforeEach(() => {
+        http = new HttpTransport();
+        request = sandbox
+            .stub(http, 'request' as keyof typeof http)
+            .callsFake(() => Promise.resolve({}));
+    });
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should blabla test case', () => {
+        http.get('', { data: { a: '1', b: '2' } });
+
+        expect(request).calledWithMatch('?a=1&b=2', 'GET');
+    });
+});
