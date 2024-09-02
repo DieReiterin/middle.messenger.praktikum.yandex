ebf6f48c9c21e9ea826cd110fedec7999c76ebed
Serafim Vasilev <zzerafim@ya.ru>
2024-09-02 09:46:21 +0300

    pachca test paths fix 4.0.5

--- src/modules/HttpTransport.test.ts
+++ src/modules/HttpTransport.test.ts
@@ -1,29 +1,27 @@
-import { expect } from 'chai';
-// import { expect, use } from 'chai';
-// import * as sinonChai from 'sinon-chai';
-// import { createSandbox, SinonStub } from 'sinon';
-// import HttpTransport from '@/modules/HttpTransport';
+import { expect, use } from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { createSandbox, SinonStub } from 'sinon';
+import HttpTransport from '@/modules/HttpTransport';
 
 describe('Http Transport test suite', () => {
-    // use(sinonChai.default);
-    // const sandbox = createSandbox();
-    // let http: HttpTransport;
-    // let request: SinonStub<any>;
+    use(sinonChai.default);
+    const sandbox = createSandbox();
+    let http: HttpTransport;
+    let request: SinonStub<any>;
 
-    // beforeEach(() => {
-    //     http = new HttpTransport();
-    //     request = sandbox
-    //         .stub(http, 'request' as keyof typeof http)
-    //         .callsFake(() => Promise.resolve({}));
-    // });
-    // afterEach(() => {
-    //     sandbox.restore();
-    // });
+    beforeEach(() => {
+        http = new HttpTransport();
+        request = sandbox
+            .stub(http, 'request' as keyof typeof http)
+            .callsFake(() => Promise.resolve({}));
+    });
+    afterEach(() => {
+        sandbox.restore();
+    });
 
     it('should blabla test case', () => {
-        // http.get('', { data: { a: '1', b: '2' } });
+        http.get('', { data: { a: '1', b: '2' } });
 
-        // expect(request).calledWithMatch('?a=1&b=2', 'GET');
-        expect({ a: 1 }).to.deep.equal({ a: 1 });
+        expect(request).calledWithMatch('?a=1&b=2', 'GET');
     });
 });
